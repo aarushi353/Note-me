95723148ce3238c8756f7fa38e42971134948247
aarushi353 <aarushiabrol999@gmail.com>
2021-08-24 22:25:43 +0530

    views added

--- ToDo_List/views.py
+++ ToDo_List/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ToDoList, Category
 
-# Create your views here.
+def index(request):
+    todos = ToDoList.objects.all()
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        if "taskIsAdded" in request.POST:
+            title = request.POST["description"]
+            date = str(request.POST["date"])
+            category = request.POST["category_select"]
+            content = title + " -- " + date + " " + category
+
+            ToDo = ToDoList(title=title, content=content,deadline=date,category=Category.objects.get(name=category))
+            ToDo.save()
+            return redirect("/")
+
+        if "taskDelete" in request.POST:
+            checklist = request.POST["checkedbox"]
+
+            for todo_id in checklist:
+                todo = ToDoList.objects.get(id=int(todo_id))
+
+                todo.delete()
+    return render(request, "index.html",{"todos": todos,"categories":categories}) 
